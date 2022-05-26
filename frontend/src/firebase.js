60c66efe9ec27c61e822079a9ac77698b41e9e31
Claudiu <clau_123@yahoo.com>
2022-05-26 14:50:21 +0300

    hide firebaseConfig

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import firebaseConfig from "../firebaseConfig";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBu7P0m47rtY5-GjIBfYjEW7kSr5qnlWhc",
-  authDomain: "lifenft-8ecb5.firebaseapp.com",
-  projectId: "lifenft-8ecb5",
-  storageBucket: "lifenft-8ecb5.appspot.com",
-  messagingSenderId: "659318632480",
-  appId: "1:659318632480:web:8a67882a76ca1abb028c45",
-};
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
